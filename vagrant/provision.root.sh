bd5f812769f8eed1bff533f7aef1a4186d7b321a
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2024-04-08 15:45:40 +0900

    ボックス化の前にクリーン処理

--- vagrant/provision.root.sh
+++ vagrant/provision.root.sh
@@ -22,3 +22,10 @@ sudo  yum  install -y  git  emacs  vim                          \
     sendmail  sudo  tcl  tk  libical   time
 
 # clean up
+sudo  yum  clean  all
+sudo  rm  -rf  /var/log/*
+sudo  rm  -rf  /tmp/*
+sudo  dd  if=/dev/zero  of=zero  bs=4k  || :
+sudo  rm  -f   zero
+
+history  -c
