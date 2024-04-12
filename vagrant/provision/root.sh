fe3603cb8d0af56ed6381115469ced83491dc429
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2024-04-12 21:27:34 +0900

    インストールするパッケージを修正
    (1) python -> python2

--- vagrant/provision/root.sh
+++ vagrant/provision/root.sh
@@ -14,8 +14,8 @@ sudo  dnf  install -y  git  emacs  vim                          \
     gcc  make  rpm-build  libtool  hwloc-devel                  \
     libX11-devel   libXt-devel  libedit-devel  libical-devel    \
     ncurses-devel  perl  postgresql-devel  postgresql-contrib   \
-    python-devel   python3-devel  tcl-devel  tk-devel  swig     \
+    python2-devel  python3-devel  tcl-devel  tk-devel  swig     \
     expat-devel    openssl-devel  libXext    libXft             \
     autoconf  automake                                          \
-    expat  libedit  postgresql-server  python  python3          \
+    expat  libedit  postgresql-server  python2  python3         \
     sendmail  sudo  tcl  tk  libical   time
