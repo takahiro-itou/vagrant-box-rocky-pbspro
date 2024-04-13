b6a8e16915859ea76f6cbabdd67fa3f7aafedbcd
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2024-04-13 12:22:53 +0900

    リファクタリング
    (1) 多分不要なパッケージのインストール手順を除去

--- vagrant/provision/root.sh
+++ vagrant/provision/root.sh
@@ -4,8 +4,8 @@ echo  Provisioning $HOSTNAME
 
 sudo  timedatectl  set-timezone Asia/Tokyo
 
-dnf install -y dnf-plugins-core
-dnf config-manager --set-enabled powertools
+# sudo  dnf install -y dnf-plugins-core
+sudo  dnf config-manager --set-enabled powertools
 
 sudo  dnf  update -y
 sudo  dnf  groupinstall -y  "Base"  "Development Tools"
