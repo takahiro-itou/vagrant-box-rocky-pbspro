94f61b09a43864cdc61ac0de73db0ca3ec803aea
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2025-11-05 19:58:03 +0900

    リファクタリング

--- vagrant/provision/setup-packages.sh
+++ vagrant/provision/setup-packages.sh
@@ -4,18 +4,50 @@ echo  Provisioning $HOSTNAME
 
 sudo  timedatectl  set-timezone Asia/Tokyo
 
-# sudo  dnf install -y dnf-plugins-core
+sudo  dnf install -y dnf-plugins-core
 sudo  dnf config-manager --set-enabled powertools
 
 sudo  dnf  update -y
 sudo  dnf  groupinstall -y  "Base"  "Development Tools"
 
-sudo  dnf  install -y  git  emacs  vim                          \
-    gcc  make  rpm-build  libtool  hwloc-devel                  \
-    libX11-devel   libXt-devel  libedit-devel  libical-devel    \
-    ncurses-devel  perl  postgresql-devel  postgresql-contrib   \
-    python2-devel  python3-devel  tcl-devel  tk-devel  swig     \
-    expat-devel    openssl-devel  libXext    libXft             \
-    autoconf  automake                                          \
-    expat  libedit  postgresql-server  python2  python3         \
-    sendmail  sudo  tcl  tk  libical   time
+sudo  dnf  install -y       \
+    git                     \
+    emacs                   \
+    vim                     \
+    \
+    gcc                     \
+    make                    \
+    rpm-build               \
+    libtool                 \
+    hwloc-devel             \
+    libX11-devel            \
+    libXt-devel             \
+    libedit-devel           \
+    libical-devel           \
+    ncurses-devel           \
+    perl                    \
+    postgresql-devel        \
+    postgresql-contrib      \
+    python2-devel           \
+    python3-devel           \
+    tcl-devel               \
+    tk-devel                \
+    swig                    \
+    expat-devel             \
+    openssl-devel           \
+    libXext                 \
+    libXft                  \
+    autoconf                \
+    automake                \
+    expat                   \
+    libedit                 \
+    postgresql-server       \
+    python2                 \
+    python3                 \
+    sendmail                \
+    sudo                    \
+    tcl                     \
+    tk                      \
+    libical                 \
+    time                    \
+;
